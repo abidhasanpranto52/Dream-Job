9cf05d7fa381e93e623a12c429e724b473668c28
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-04-12 12:19:35 +0600

    create react-router & components added

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            This is statiscs page
+        </div>
+    );
+};
+
+export default Statistics;
---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Jobs from './components/Jobs/Jobs';
+import Blog from './components/Blog/Blog';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "statistics",
+        element: <Statistics />
+      },
+      {
+        path: "jobs",
+        element: <Jobs />
+      },
+      {
+        path: "blog",
+        element: <Blog />
+      },
+
+    ]
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)

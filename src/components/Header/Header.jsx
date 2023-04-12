932c59636698227b374e6b5f65654a970d064446
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-04-12 14:05:30 +0600

    home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,121 +9,124 @@ import {
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
-        <div className='flex items-center justify-between'>
-            {/*Logo */}
-            <Link to='/' className='inline-flex items-center'>
-                <BoltSlashIcon className='h-6 w-6' />
-                <span>
-                    <h4 className='text-3xl font-bold'>Next page</h4>
-                </span>
-            </Link>
+        <div className=' px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+            <div className='relative flex items-center justify-around'>
+                {/*Logo */}
+                <Link to='/' className='inline-flex items-center'>
+                    <BoltSlashIcon className='h-7 w-7 text-amber-800' />
+                    <span>
+                        <h4 className='text-3xl font-bold'>Next Project</h4>
+                    </span>
+                </Link>
 
-
-            <ul className='items-center hidden space-x-8 lg:flex'>
-                <li>
-                    <NavLink
-                        to='/'
-                        className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
+                {/* Navbar Section */}
+                <ul className='items-center hidden space-x-8 lg:flex'>
+                    <li>
+                        <NavLink
+                            to='/'
+                            className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/statistics'
+                            className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
+                        >
+                            Statistics
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/jobs'
+                            className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
+                        >
+                            Applied Jobs
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/blog'
+                            className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
+                        >
+                            Blog
+                        </NavLink>
+                    </li>
+                </ul>
+                    <button class="rounded text-white bg-indigo-500">Start Applying</button>
+                {/* Mobile Navbar Section */}
+                <div className='lg:hidden'>
+                    {/* Dropdown Open Button */}
+                    <button
+                        aria-label='Open Menu'
+                        title='Open Menu'
+                        onClick={() => setIsMenuOpen(true)}
                     >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/statistics'
-                        className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
-                    >
-                        Statistics
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/jobs'
-                        className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
-                    >
-                        Applied Jobs
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to='/blog'
-                        className={({ isActive }) => (isActive ? 'text-blue-700' : '')}
-                    >
-                        Blog
-                    </NavLink>
-                </li>
-            </ul>
-            {/* Mobile Navbar Section */}
-            <div className='lg:hidden'>
-                {/* Dropdown Open Button */}
-                <button
-                    aria-label='Open Menu'
-                    title='Open Menu'
-                    onClick={() => setIsMenuOpen(true)}
-                >
-                    <Bars3BottomRightIcon className='w-5 text-gray-600' />
-                </button>
-                {isMenuOpen && (
-                    <div className='absolute top-0 left-0 w-full z-10'>
-                        <div className='p-5 bg-white border rounded shadow-sm'>
-                            {/* Logo & Button section */}
-                            <div className='flex items-center justify-between mb-4'>
-                                <div>
-                                    <Link to='/' className='inline-flex items-center'>
-                                        <BoltSlashIcon className='h-6 w-6 text-blue-500' />
-                                        <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
-                                            nextPage
-                                        </span>
-                                    </Link>
+                        <Bars3BottomRightIcon className='w-5 text-gray-600' />
+                    </button>
+                    {isMenuOpen && (
+                        <div className='absolute top-0 left-0 w-full z-10'>
+                            <div className='p-5 bg-white border rounded shadow-sm'>
+                                {/* Logo & Button section */}
+                                <div className='flex items-center justify-between mb-4'>
+                                    <div>
+                                        <Link to='/' className='inline-flex items-center'>
+                                            <BoltSlashIcon className='h-6 w-6 text-amber-800' />
+                                            <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                                                Next-Project
+                                            </span>
+                                        </Link>
+                                    </div>
+                                    {/* Dropdown menu close button */}
+                                    <div>
+                                        <button
+                                            aria-label='Close Menu'
+                                            title='Close Menu'
+                                            onClick={() => setIsMenuOpen(false)}
+                                        >
+                                            <XMarkIcon className='w-5 text-gray-600' />
+                                        </button>
+                                    </div>
                                 </div>
-                                {/* Dropdown menu close button */}
-                                <div>
-                                    <button
-                                        aria-label='Close Menu'
-                                        title='Close Menu'
-                                        onClick={() => setIsMenuOpen(false)}
-                                    >
-                                        <XMarkIcon className='w-5 text-gray-600' />
-                                    </button>
-                                </div>
+                                {/* Mobile Nav Items Section */}
+                                <nav>
+                                    <ul className='space-y-4'>
+                                        <li>
+                                            <Link to='/' className='default'>
+                                                Home
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to='/statistics'
+                                                className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                            >
+                                                Statistics
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to='/jobs'
+                                                className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                            >
+                                                Applied Jobs
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to='/blog'
+                                                className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                            >
+                                                Blog
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </nav>
                             </div>
-                            {/* Mobile Nav Items Section */}
-                            <nav>
-                                <ul className='space-y-4'>
-                                    <li>
-                                        <Link to='/' className='default'>
-                                            Home
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            to='/statistics'
-                                            className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
-                                        >
-                                            Statistics
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            to='/jobs'
-                                            className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
-                                        >
-                                            Applied Jobs
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            to='/blog'
-                                            className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
-                                        >
-                                            Blog
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </nav>
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from 'react'
+import Lottie from 'lottie-react'
+import programming from '../Header/programming.json'
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className='my-container mx-auto flex flex-col items-center justify-around lg:flex-row'>
+            {/* Text Content */}
+            <div className='mb-10 lg:max-w-lg  lg:pr-5 lg:mb-0'>
+                <div className='max-w-xl mb-6 lg:mt-8'>
+                    
+                    <h1 className='text-5xl font-bold text-black mb-3'>One Step <br /> Closer To Your <br /><span className='text-indigo-700'> Dream Job</span></h1>
+                    <p className='text-base text-gray-700 md:text-lg'>
+                    Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
+                    </p>
+                </div>
+                <div className='flex flex-col items-center md:flex-row'>
+                    <Link to='/statistics' className='btn rounded p-3 text-white md:w-auto md:mr-4 bg-indigo-500'>
+                        <div className='inline-flex items-center justify-center w-full h-full'>
+                            <p className='mr-2'>Get Started</p>
+                            <ArrowRightIcon className='w-5 text-gray-100' />
+                        </div>
+                    </Link>
+                </div>
+            </div>
+            {/* Lottie Animation */}
+            <div className='relative lg:w-1/2 '>
+                <div className='w-full lg:w-4/5 lg:ml-auto h-56  sm:h-96'>
+                    <Lottie animationData={programming} loop={true} />
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
